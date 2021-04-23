5e52d61fbe503e1c5c0539b3b200a6f3902b8cd8
Moisés Adamuz <moises.adamuz.fernandez@gmail.com>
2021-04-23 07:34:43 +0200

    Primer Guardado sin tocar nada de la App

---
+++ Kairos/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Kairos.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Kairos.Views {
+    public partial class ItemDetailPage : ContentPage {
+        public ItemDetailPage() {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Kairos/Views/NewItemPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Kairos.Models;
+using Kairos.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Kairos.Views {
+    public partial class NewItemPage : ContentPage {
+        public Item Item { get; set; }
+
+        public NewItemPage() {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
